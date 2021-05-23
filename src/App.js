7ef7f86a83d11f4d8467f5a60ccc519c7a91bd60
iMMoCodes <matti-makela@hotmail.com>
2021-05-23 14:37:40 +0300

    Created floating box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import {Canvas} from "@react-three/fiber"
+import {OrbitControls, Stars} from "@react-three/drei"
+
+function Box () {
+    return(
+        <mesh position={[0,1,0]}>
+            <boxBufferGeometry attach="geometry"/>
+            <meshLambertMaterial attach="material" color="hotpink"/>
+        </mesh>
+    )
+}
+
+function Plane () {
+    return(
+        <mesh position={[0,0,0]} rotation={[-Math.PI/2,0,0]}>
+            <planeBufferGeometry attach="geometry" args={[100,100]}/>
+            <meshLambertMaterial attach="material" color="lightblue"/>
+        </mesh>
+    )
+}
 
 const App = () => {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <Canvas style={{height:"98vh"}}>
+            <OrbitControls/>
+            <Stars/>
+            <ambientLight intensity={0.5}/>
+            <spotLight position={[10,15,10]} angle={0.3}/>
+            <Box/>
+            <Plane/>
+        </Canvas>
     )
 }
 
